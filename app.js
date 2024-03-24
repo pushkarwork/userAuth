64bee2dea857a5da322fe0007df73a06e1b2cc01
pushkar verma <pushkar753@gmail.com>
2024-03-24 20:02:25 +0530

    cors installed

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+const cors = require("cors")
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -17,6 +18,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
